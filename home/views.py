cb8658e4dc7d1820227ba16ff381f755e77553c7
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2020-07-20 19:11:54 +0530

    done

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def search(request):
+    return render(request, 'search.html')
